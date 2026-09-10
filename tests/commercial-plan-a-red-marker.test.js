ff6c0b0b7557a6b9e1c87492cd5c681950dc748c
dharan <150376358+dharan1007@users.noreply.github.com>
2026-09-10 13:13:03 +0530

    test: remove temporary RED marker

--- tests/commercial-plan-a-red-marker.test.js
+++
@@ -1,6 +0,0 @@
-import test from 'node:test';
-import assert from 'node:assert/strict';
-
-test('Plan A RED boundary is exercised by commercial foundation tests',()=>{
-  assert.equal(true,true);
-});
